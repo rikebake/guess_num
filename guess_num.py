2a9c8b32b54e4fa465c36391dfa90d31a684d968
Richard Tsai <rikebake@mail2000.com.tw>
2022-12-27 17:26:12 +0800

    added guess count

--- guess_num.py
+++ guess_num.py
@@ -1,13 +1,15 @@
 import random 
-
+guess = 0 # the #of guess
 r = random.randint(1,100)
 while True:
 	user_guess = int(input('input a integer '))# the number that user guesses
+	guess += 1#guess = guess + 1
 	if user_guess == r:
-		print('correct!')
+		print('correct!','total ',guess,'times of guess')
 		break
 	elif user_guess > r:
 		print('larger ')
 	elif user_guess < r:
 		print('smaller')
+	print(guess,'times')
 
