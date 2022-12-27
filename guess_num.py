9e82c469002190ddf51df05199435314e6b44eb6
Richard Tsai <rikebake@mail2000.com.tw>
2022-12-27 17:38:42 +0800

    added options for range-upper/lower

--- guess_num.py
+++ guess_num.py
@@ -1,6 +1,14 @@
 import random 
+while True: # to avoid upper < lower
+	upper_rand = int(input('input the upper limit of the random number '))
+	lower_rand = int(input('input the lower limit of the random number '))
+	if upper_rand > lower_rand:
+		break
+	else:
+		print('upper must larger than lower')
+
 guess = 0 # the #of guess
-r = random.randint(1,100)
+r = random.randint(lower_rand,upper_rand)
 while True:
 	user_guess = int(input('input a integer '))# the number that user guesses
 	guess += 1#guess = guess + 1
